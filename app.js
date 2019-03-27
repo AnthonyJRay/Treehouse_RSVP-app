15c99676f616e1496050bafbebee0b874ef47b34
Anthony Eriksen <ajrayid@gmail.com>
2019-03-27 16:57:17 -0700

    Created a function createLi
    Created a function to break up the code to be more readable.

--- app.js
+++ app.js
@@ -1,10 +1,8 @@
 const form = document.getElementById('registrar');      // Assigning the form element to 'form' variable.
 const input = document.querySelector('input');          // Assigning the input to 'input' variable.
 const ul = document.getElementById('invitedList');      // Stores the ul with id 'invitedList' to variable 'ul'
-form.addEventListener('submit', (e) => {                // Starts an event to listen for a 'submit' action.
-    e.preventDefault();                                 // Preventing the default browser behavior from refreshing the page upon submit.
-    const text = input.value;                           // Stores the user input value in variable 'text'
-    input.value = '';                                   // Clears input field after submitting a name.  
+
+function createLi(text) {
     const li = document.createElement('li');            // Creates a new 'li' item
     li.textContent = (text);                            // Sets the content of the 'li' item to the 'text' input.value
     const label = document.createElement('label');      // Creates a 'label' element.
@@ -16,6 +14,14 @@ form.addEventListener('submit', (e) => {                // Starts an event to li
     const button = document.createElement('button');    // Creates a button element with variable name button
     button.textContent = 'remove';                      // Sets the buttons content to be removed       
     li.appendChild(button);                             // Appends the button to the li element.
+    return li;
+}
+
+form.addEventListener('submit', (e) => {                // Starts an event to listen for a 'submit' action.
+    e.preventDefault();                                 // Preventing the default browser behavior from refreshing the page upon submit.
+    const text = input.value;                           // Stores the user input value in variable 'text'
+    input.value = '';                                   // Clears input field after submitting a name. 
+    const li = createLi(text); 
     ul.appendChild(li);                                 // Appends the li item to the ul element.                            
 });
 
