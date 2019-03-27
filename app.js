1fc53b9dc38bf2a7e7c72815ae031d5da649d726
Anthony Eriksen <ajrayid@gmail.com>
2019-03-27 15:58:43 -0700

    Started list functionality
    Added list functionality with commented notes.

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
-const form = document.getElementById('registrar');
-const input = document.querySelector('input');
+const form = document.getElementById('registrar');      // Assigning the form element to 'form' variable.
+const input = document.querySelector('input');          // Assigning the input to 'input' variable.
 
-form.addEventListener('submit', (e) => {
-  console.log(input.value);
+form.addEventListener('submit', (e) => {                // Starts an event to listen for a 'submit' action.
+    e.preventDefault();                                 // Preventing the default browser behavior from refreshing the page upon submit.
+    const text = input.value;                           // Stores the user input value in variable 'text'
+    input.value = '';                                   // Clears input field after submitting a name.
+    const ul = document.getElementById('invitedList');  // Stores the ul with id 'invitedList' to variable 'ul'
+    const li = document.createElement('li');            // Creates a new 'li' item
+    li.textContent = (text);                            // Sets the content of the 'li' item to the 'text' input.value
+    ul.appendChild(li);                                 // Appends the created 'li' item to the 'ul' element.
 });
