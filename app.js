f0cb55f7cb6458459cf6cd8b057ecdf8f38d93a5
Anthony Eriksen <ajrayid@gmail.com>
2019-03-27 20:16:27 -0700

    Added editing capabilities
    Added an edit state and a save button to the invitee's list items.

--- app.js
+++ app.js
@@ -4,24 +4,31 @@ const ul = document.getElementById('invitedList');      // Stores the ul with id
 
 function createLi(text) {
     const li = document.createElement('li');            // Creates a new 'li' item
-    li.textContent = (text);                            // Sets the content of the 'li' item to the 'text' input.value
+    const span = document.createElement('span');        // Creates span element
+    span.textContent = (text);                          // Sets the content of the span element to a 'text' input.value
+    li.appendChild(span);                               // Appends the span to li
     const label = document.createElement('label');      // Creates a 'label' element.
     label.textContent = 'Confirmed';                    // Adds the text 'Confirmed' to label element.
     const checkbox = document.createElement('input');   // Creates an input variable named 'checkbox'.
     checkbox.type = 'checkbox';                         // Sets the variable 'checkbox' type to 'checkbox'
     label.appendChild(checkbox);                        // Appends the checkbox to the label element.
     li.appendChild(label);                              // Appends the label element to the li.
-    const button = document.createElement('button');    // Creates a button element with variable name button
-    button.textContent = 'remove';                      // Sets the buttons content to be removed       
-    li.appendChild(button);                             // Appends the button to the li element.
-    return li;
+
+    const editButton = document.createElement('button');    // Creates a button for edit
+    editButton.textContent = 'edit';                        // Sets the content of the button to 'edit'
+    li.appendChild(editButton);                             // Appends the editButton to the li element.
+
+    const removeButton = document.createElement('button');    // Creates a button element with variable name button
+    removeButton.textContent = 'remove';                      // Sets buttons text to 'remove'       
+    li.appendChild(removeButton);                             // Appends the button to the li element.
+    return li;                                                // Returns the function li
 }
 
 form.addEventListener('submit', (e) => {                // Starts an event to listen for a 'submit' action.
     e.preventDefault();                                 // Preventing the default browser behavior from refreshing the page upon submit.
     const text = input.value;                           // Stores the user input value in variable 'text'
     input.value = '';                                   // Clears input field after submitting a name. 
-    const li = createLi(text); 
+    const li = createLi(text);                          // Stores the createLi function in a variable named li
     ul.appendChild(li);                                 // Appends the li item to the ul element.                            
 });
 
@@ -39,8 +46,26 @@ ul.addEventListener('change', (e) => {                  // Added an event listen
 
 ul.addEventListener('click', (e) => {                   // Adds an event listener to the ul to listen for clicks.
     if (e.target.tagName == 'BUTTON') {                 // If the target is a button with a ul
-        const li = e.target.parentNode;                 // Sets the li to the child of ul                     
-        const ul = li.parentNode;                       // Sets the ul in a variable as a parent to li      
+        const button = e.target;                        // Set e.target in variable button for better readability
+        const li = button.parentNode;                   // Sets the li to the child of ul                     
+        const ul = li.parentNode;                       // Sets the ul in a variable as a parent to li  
+        if (button.textContent == 'remove') {           // Added if content == remove to distinguish between edit and remove buttons.   
         ul.removeChild(li);                             // Remove the li child element of ul
-    }
+    } else if (button.textContent == 'edit') {          // Or else if the button content is equal to 'edit'
+        const span = li.firstElementChild;              // Selects the span using the first child of li
+        const input = document.createElement('input');  // Creates input element to replace span with
+        input.type = 'text';                            // Sets the input type to be text
+        input.value = span.textContent;                 // Sets the value of the input element to the textContent of the span.
+        li.insertBefore(input, span);                   // Inserts the span element into the DOM, with input coming before.
+        li.removeChild(span);                           // Removes the span to remove the text.
+        button.textContent = 'save';                    // Changes the button text from 'Edit' to 'Save'
+    } else if (button.textContent == 'save') {          // -----------------------------------------
+        const input = li.firstElementChild;             //
+        const span = document.createElement('span');    //  Does the exact same as the code block above,
+        span.textContent = input.value;                 //  just reversed the input and span elements,
+        li.insertBefore(span, input);                   //  to create a 'save' button while in an edit state,
+        li.removeChild(input);                          //  in order to save changes and exit the edit state.
+        button.textContent = 'edit';                    //------------------------------------------
+        }
+    }    
 });
